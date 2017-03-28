b41b7d6aed66675c802364d35cb6b1fd94321ec6
jianjunxu <303583002@qq.com>
2017-03-28 13:09:35 +0800

    fix

---
+++ src/main/java/com/jayden/basic/FileTest.java
@@ -0,0 +1,25 @@
+package com.jayden.basic;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ * Created by za-xujianjun on 2017/3/28.
+ */
+public class FileTest {
+    public static void main(String[] args) {
+        File[] hiddenFiles7 = new File(".").listFiles(new FileFilter() {
+            public boolean accept(File pathname) {
+                return pathname.isHidden();
+            }
+        });
+        for (File file : hiddenFiles7) {
+            System.out.println("jdk7---" + file.getName());
+        }
+        // 方法引用（::） 将方法作为值传递
+        File[] hiddenFiles8 = new File(".").listFiles(File::isHidden);
+        for (File file : hiddenFiles8) {
+            System.out.println("jdk8---" + file.getName());
+        }
+    }
+}
