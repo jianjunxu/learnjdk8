a45ad4d5c1c0f2f987f09b27e5dc796bbf801f0d
jianjunxu <303583002@qq.com>
2017-03-29 14:07:57 +0800

    npe

--- src/main/java/com/jayden/basic/spi/SearchFactory.java
+++ src/main/java/com/jayden/basic/spi/SearchFactory.java
@@ -1,20 +1,23 @@
 package com.jayden.basic.spi;
 
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.ServiceLoader;
 
 /**
  * Created by za-xujianjun on 2017/3/28.
  */
 public class SearchFactory {
-    private SearchFactory(){}
-    public static Search newSearch(){
+    private SearchFactory() {
+    }
+
+    public static Search newSearch() {
         Search search = null;
         ServiceLoader<Search> loader = ServiceLoader.load(Search.class);
         Iterator<Search> iters = loader.iterator();
-        if(iters.hasNext()){
+        if (iters.hasNext()) {
             search = iters.next();
         }
-        return search;
+        return Objects.requireNonNull(search, "config error.");
     }
 }
